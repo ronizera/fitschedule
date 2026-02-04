4bf94fb34dc24634effdc766646efe624fd2ab7c
ronizera <roniltonbreno@gmail.com>
2026-02-03 22:27:44 -0300

    fix logout bugs

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,20 +2,24 @@ import { prisma } from "@/lib/prisma";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: Request) {
     const cookieStore = await cookies()
     const sessionId = cookieStore.get("sessionId")?.value
 
     if(sessionId){
+        try{
+
+        
         await prisma.session.delete({
             where: {id: sessionId}
         })
+        }catch (e) {
+            console.error("Sessao encerrada ou inexistente")
+        }
     }
 
-    const response = NextResponse.json(
-        {message: "Logout realizado"},
-        {status: 200}
-    )
+    const url = new URL("/", request.url);
+    const response = NextResponse.redirect(url)
 
     response.cookies.set("sessionId", "", {
         path: "/",
